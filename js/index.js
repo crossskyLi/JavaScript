bcd9d305758dca8b26ad62737d50c89b62159360
Rambo <liguanliang@boyue.com>
2018-01-30 18:15:33 +0800

    class 继承

--- js/index.js
+++ js/index.js
@@ -9030,6 +9030,127 @@ $(function () {
     //-----------------------------------------------------------------------
     //------------------ Class 的继承  ---------------------
 
+    // 1.简介
+    // Class 可以通过extends 关键字实现继承,比ES5通过修改原形链实现继承,更加清晰方便
+    // class Point{}
+    // class ColorPoint extends Point{
+    //
+    // }
+    // 代码中定义了一个Color类,该类通过extends关键字,继承了Point的所有属性和方法
+    // 但是由于没有部署任何代码,所以两个类完全一样,相当于复制了一个Point类
+
+    // class Point{
+    //     constructor(x,y){
+    //         this.a = x;
+    //         this.b = y;
+    //     }
+    //     toString(){
+    //         return '父类'
+    //     }
+    // }
+    // class ColorPoint extends Point{
+    //     constructor(x,y,color){
+    //         super(x,y);
+    //         this.color = color;
+    //     }
+    //     toString(){
+    //         console.log(super.toString());
+    //         return this.color+'   ' + super.toString();
+    //     }
+    // }
+    // let result = new ColorPoint(1,2,3);
+    // console.log(result);
+    // let str = result.toString();
+    // console.log(str);
+    // 代码中,constructor方法和toString方法中,都出现了super关键字,
+    // 它在这里表示父类的构造函数,用来新建父类的this对象
+
+    // 子类必须在constructor方法中调用super方法,
+    // 否则新建实例时,会报错,这是因为子类没有自己的this对象
+    // 而是继承父类的this对象,然后在对其进行加工,如果不调用super方法,
+    // 子类就得不到this对象
+    // class Point {
+    //     //...
+    // }
+    // class ColorPoint extends Point{
+    //     constructor(){
+    //         // super()// 不加的话会报错
+    //     }
+    // }
+    // let cp = new ColorPoint(); //ReferenceError Must call super constructor
+    // // in derived class before accessing 'this' or returning from derived constructor
+    // console.log(cp)
+    // 代码中,colorPoint 继承了父类Point ,
+    // 但是它的构造函数没有调用super方法,导致新建实例报错
+
+    // ES5 的继承,实质是先创造子类的实例对象this,
+    // 然后再将父类的方法添加到this上面(Parent.apply(this))
+    // ES6 的继承机制完全不同,实质上是先创造父类的实例对象this(所以必须先调用super方法)
+    // 然后再用子类的构造函数修改this
+
+    // 如果子类没有定义 constructor方法,这个方法会被默认添加,
+    // 代码如下,也就是说,不管有没有显式定义,任何一个子类都有constructor方法
+    // class ColorPoint extends Point{
+    //
+    // }
+    // // 等同于
+    // class ColorPoint extends Point{
+    //     constructor(...args){
+    //         super(...args)
+    //     }
+    // }
+
+    // 注意,在子类的构造函数中,至于调用super之后,才可以使用this关键字,
+    // 否则会报错,这是因为子类实例的构建,是基于父类实例加工,
+    // 只有super方法才能返回父类实例
+    // class Point {
+    //     constructor(x,y){
+    //         this.x = x;
+    //         this.y = y;
+    //     }
+    // }
+    // class ColorPoint extends Point{
+    //     constructor(x,y,color){
+    //         // this.color = color; // 报错
+    //         // Must call super constructor in derived
+    //         // class before accessing 'this' or returning from derived constructor ReferenceError
+    //         super(x,y);
+    //         this.color = color;
+    //     }
+    // }
+    // let result = new ColorPoint(12,1,1);
+    // 代码中,子类的constructor方法没有调用super之前,
+    // 就使用this关键字,导致报错,而放在super方法之后就正确
+
+    // 下面是生成子类实例的代码
+    // class Point {
+    //     constructor(x,y){
+    //         this.x = x;
+    //         this.y = y;
+    //     }
+    // }
+    // class ColorPoint extends Point{
+    //     constructor(x,y,color){
+    //         // this.color = color; // 报错
+    //         // Must call super constructor in derived
+    //         // class before accessing 'this' or returning from derived constructor ReferenceError
+    //         super(x,y);
+    //         this.color = color;
+    //     }
+    // }
+    // let cp = new ColorPoint(20,12,'green');
+    // console.log(cp instanceof ColorPoint); // true
+    // console.log(cp instanceof Point)// true
+    // 代码中,实例对象cp同时是ColorPoint 和Point 两个类的实例
+    // 这与ES5 行为完全一致
+
+
+
+
+
+
+
+
 
 
     // function timeCount() {
