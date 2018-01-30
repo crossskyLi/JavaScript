217264ab553de6c3260c108f0588938ed4180b2a
Rambo <liguanliang@boyue.com>
2018-01-30 21:00:32 +0800

    super 关键字

--- js/index.js
+++ js/index.js
@@ -9187,17 +9187,181 @@ $(function () {
 
     // 第一种,super作为函数调用,代表父类的构造函数,
     // es6要求,子类的构造函数必须执行一次super函数
-    class A{}
-    class B extends A {
-        constructor(){
-            super()
-        }
-    }
+    // class A{}
+    // class B extends A {
+    //     constructor(){
+    //         super()
+    //     }
+    // }
     // 代码中,子类B 的构造函数中super(), 代表调用父类的构造函数
     // 这是必须的,否则javascript引擎会报错
 
     // 注意super 虽然代表父类A 的构造函数,但是返回的是子类B的实例
     // 即super内部的this指的是B,因此super() 在这里相当于,A.prototype.constructor.call(this)
+
+    // class A{
+    //     constructor(){
+    //         console.log(new.target.name)
+    //     }
+    // }
+    // class B extends A {
+    //     constructor(){
+    //         super();
+    //     }
+    // }
+    // let result = new A(); // A
+    // let resultB = new B(); // B
+    // console.log(result ,resultB)
+    // 代码中,new.target 指向当前正在执行的函数,可以看到super 在执行时
+    // 它指向的是子类B的构造函数,而不是父类A的构造函数,
+    // 也就是说super内部的this指向的是B
+
+    // 作为函数时,super() 只能用在子类的构造函数之中,
+    // 用在其他地方会报错
+    // class A {}
+    // class B extends A {
+    //     m(){
+    //         super(); // 报错 'super' keyword unexpected here
+    //     }
+    // }
+    // 代码中super() 用在B类的m方法中,会造成语法错误
+
+    // 第二种情况
+    // super 作为对象时,在普通方法中,指向父类的原型对象,
+    // 在静态方法中,指向父类
+    // class A {
+    //     p(){
+    //         return 2
+    //     }
+    // }
+    // class B extends A {
+    //     constructor(){
+    //         super();
+    //         console.log(super.p()) // 2
+    //     }
+    // }
+    //
+    // let b = new B();
+    // console.log(b);
+    // 代码中,子类B 当中super.p(),就是将super当作一个对象使用
+    // 这时候 super 在普通方法中,指向A.prototype,
+    // 所以super.p()就相当于A.prototyoe.p()
+    // 注意: 由于super 指向父类的原型对象,所以定义在父类实例上的方法或属性
+    // 是无法通过super 调用的
+    // class A {
+    //     constructor(){
+    //         this.p = 2;
+    //     }
+    // }
+    // class B extends A {
+    //     get m(){
+    //         return super.p;
+    //     }
+    // }
+    // let b = new B();
+    // console.log(b); // {p:2}
+    // console.log(b.__proto__); // A {constructor: ƒ}
+    // console.log(b.m) //  undefined
+    // 代码中,p 是父类A实例的属性,super.p 引用不到它
+    // 如果属性定义在父类的原型对象上,super就可以取到
+
+    // class A {}
+    // A.prototype.x = 2;
+    // class B extends A {
+    //     constructor(){
+    //         super();
+    //         console.log(super.x) //2
+    //     }
+    // }
+    // let b = new B();
+    // console.log(b);
+    // console.log('b.x',b.x) // 2
+    // 代码中,属性x 是定义在A.prototype上面的,所以super.x 可以取到它的值
+    // ES6 规定,通过super 调用父类的方法时,方法内部this 指向子类
+    // class A {
+    //     constructor(){
+    //         this.x = 1;
+    //     }
+    //     print (){
+    //         console.log(this.x);
+    //     }
+    // }
+    // class B extends A {
+    //     constructor(){
+    //         super();
+    //         this.x = 2;
+    //     }
+    //     m (){
+    //         super.print();
+    //     }
+    // }
+    // let a = new A();
+    // let b = new B();
+    // console.log(a); //{x:1}
+    // console.log(b); //{x:2}
+    // b.m(); // 2  this 指向 B
+    // 代码中,super.print(),虽然调用的是A.prototype.print();
+    // 但是A.prototype.print 内部的this 指向子类this,导致输出的是2
+    // 而不是1 也就是说,实际上执行的是super.print.call(this)
+    // 个人理解,在去掉B 中constructor中this.x = 2后,b.m()输出的是1
+    // 那么也就是说在原形链一直找x,直到找到父类的x,就打印出来
+
+    // 由于this 指向子类,所以如果通过super 对某个属性赋值,这时候super 就是this,
+    // 赋值的属性会变成子类实例的属性
+    // class A {
+    //     constructor() {
+    //         this.x = 1;
+    //     }
+    // }
+    // A.prototype.x = 'a prototype的x';
+    // class B extends A {
+    //     constructor() {
+    //         super();
+    //         this.x = 3;
+    //         super.x = 10;
+    //         console.log(super.x); // undefined
+    //         console.log(this.x); // 10
+    //     }
+    // }
+    // let b = new B();
+    // console.log(b);// B{x :10}
+    // console.log(b.x) // 10
+    // 上面代码中, super.x赋值为10,这等同于this.x 赋值为10
+    // 而当读取super.x 的时候,读的是A.prototype.x 所以返回的是"a prototype的x"
+
+    // super 作为对象,在静态方法中,这时super将指向父类,
+    // 而不是父类的原型对象
+    class Parent {
+        static myMethod(msg){
+            console.log('static',msg)
+        }
+        myMethod(msg){
+            console.log('instance',msg)
+        }
+    }
+    class Child extends Parent{
+        static myMethod(msg){
+            super.myMethod(msg);
+        }
+        myMethod(msg){
+            super.myMethod(msg)
+        }
+    }
+    Child.myMethod('子类调用父类的静态方法');
+    let child = new Child();
+    child.myMethod('子类实例调用父类的实例方法');
+    // 代码中,super 在静态方法中,指向父类,在普通方法中指向父类的原型对象
+    // 注意,使用super的时候,必须显式指定是作为函数,还是作为对象使用,否则会报错
+    // class A {}
+    // class B extends A{
+    //     constructor(){
+    //         super();
+    //         console.log(super);//报错 'super' keyword unexpected here
+    //     }
+    // }
+
+
+
 
 
 
