4713f9b5726bdc106063129a898bc211119ca211
Rambo <liguanliang@boyue.com>
2018-02-05 18:02:38 +0800

    class 继承

--- js/index.js
+++ js/index.js
@@ -9689,6 +9689,46 @@ $(function () {
     // 一旦发现Object方法不是通过new Object() 这种形式调用的,
     // ES6规定Object构造函数会忽略参数
 
+    // 6.Mixin 模式的实现
+    // Mixin 指的是多个对象合成一个新的对象,
+    // 新对象具有各个组成成员的接口,简单实现如下
+    // const a = {
+    //     a:'123'
+    // };
+    // const b = {
+    //     b:'321'
+    // };
+    // const c = {...a,...b};
+    // console.log(c)
+    // 代码中,c对象是a对象和b对象的合成,具有两者的接口
+
+    // 更完备的实现,将多个类的接口 '混入'(mix in),另一个类
+    // function mix(...mixins) {
+    //     class Mix {}
+    //     for(let mixin of mixins){
+    //         copyProperties(Mix,mixin);// 拷贝实例属性
+    //         copyProperties(Mix.prototype,mixin,prototype)
+    //     }
+    //     return Mix;
+    // }
+    // function copyProperties(target, source) {
+    //     let dest = {};
+    //     for(let key of Reflect.ownKeys(source)){
+    //         if(key !== 'constructor'&& key !== 'prototype' && key !== 'name'){
+    //             dest = Object.getOwnPropertyDescriptor(source,key);
+    //             Object.defineProperty(target,key,desc)
+    //         }
+    //     }
+    // }
+    // 上面代码的mix函数,可以将多个对象合成为一个类,使用的时候,继承这个类即可
+    // class DistributedEdit extends mix (Loggable,Serializable){
+    //     //...
+    // }
+    //---------------------------------------------------
+
+
+
+
 
 
 
