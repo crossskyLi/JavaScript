7d28a0b464a4b1ac954f9e344c2c6a265d200cb7
Rambo <liguanliang@boyue.com>
2018-02-05 11:54:40 +0800

    extends 的继承目标

--- js/index.js
+++ js/index.js
@@ -9404,6 +9404,144 @@ $(function () {
     // 指向对应的构造函数的prototype属性。
     // Class 作为构造函数的语法糖,同时有prototype属性和__proto__属性
     // 因此同时存在两条继承链
+    // class A {}
+    // let a = new A();
+    // console.log('A.prototype',A.prototype);
+    // console.log('A.__proto__',A.__proto__);
+    // console.log('----------')
+    // console.log('a.prototype',a.prototype);
+    // console.log('a.__proto__',a.__proto__);//{constructor: ƒ}constructor: class A __proto__: Object
+    // (1) 子类的__proto__属性,表示构造函数的继承,总是指向父类
+    // (2) 子类的prototype属性的__proto__属性,表示方法的继承,总是指向父类的prototype属性
+    // class A{
+    //     getAStr(){
+    //         console.log('aaa123');
+    //         return 'aaa'
+    //     }
+    // }
+    // class B extends A {
+    //     getStr(){
+    //         console.log('123');
+    //         return '321'
+    //     }
+    // }
+    // console.log(B.__proto__ === A); // true
+    // console.log(B.prototype.__proto__ === A.prototype);// true
+    // console.log(B.prototype);
+    // console.log(B.prototype.getAStr());
+    // console.log(B.prototype.getStr());
+    // let result = new B();
+    // console.log(result);
+    // result.getStr();
+    // result.getAStr();
+    // 代码中,子类B的__proto__属性指向父类A ,
+    // 子类B的prototype属性的__proto__属性指向父类A 的prototype属性
+
+    // 这样子结果是因为,类的继承是按照下面模式实现的
+    // class A {
+    //
+    // }
+    // class B{
+    //
+    // }
+    // console.log(B.prototype,A.prototype);
+    // // B 的实例继承A的实例
+    // Object.setPrototypeOf(B.prototype,A.prototype);
+    //
+    // console.log(B,A);
+    // // B的实例继承A 的静态属性
+    // Object.setPrototypeOf(B,A);
+    // console.log(B.__proto__ === A); // true
+    // console.log(B.prototype.__proto__ === A.prototype);// true
+    // const b = new B();
+    // console.log(b);
+    //《对象的扩展》一章给出过Object.setPrototypeOf方法的实现。
+    // Object.setPrototypeOf = function (obj, proto) {
+    //     obj.__proto__ = proto;
+    //     return obj
+    // }
+    // 得出上面结果
+
+    // Object.setPrototypeOf(B.prototype,A.prototype);
+    // // 等同于
+    // B.prototype.__proto__ = A.prototype;
+    //
+    // Object.setPrototypeOf(B,A);
+    // // 等同于
+    // B.__proto__ = A;
+    // 这两条继承继承链可以这样理解:
+    // 作为一个对象,子类(B)的原型(__proto__属性)是父类(A)
+    // 作为一个构造函数,子类(B)的原型对象(prototype属性)是父类的原型对象(prototype属性)的实例
+    // Object.create(A.prototype);
+    // // 等同于
+    // B.prototype.__proto__ = A.prototype
+
+    //------------------
+    // extends 的继承目标
+    // extends 关键字后面可以跟多种类型的值
+    // class B extends A {
+    //
+    // }
+    // 代码中,只要是一个有prototype属性的函数, 就能被B继承
+    // 由于函数都有prototype属性,(除了 Function.prototype函数),因此A可以是任意函数
+
+    // 三种特殊情况
+    // 第一种,子类继承Object类
+    // class A extends Object{
+    //
+    // }
+    // console.log(A.__proto__ === Object); // true
+    // console.log(A.prototype.__proto__ === Object.prototype)// true
+    // 这种情况,A其实就是构造函数Object的复制,A 的实例就是Object的实例
+
+    // 第二种,不存在任何继承
+    // class A{
+    //
+    // }
+    // console.log(A.__proto__ === Function.prototype) // true
+    // console.log(A.prototype.__proto__ === Object.prototype)// true
+    // 这种情况下,A 作为一个基类,(即不存在任何继承), 就是一个普通函数
+    // 所以直接继承 Function.prototype, 但是A 调用后返回一个空对象(即Object实例)
+    // 所以 A.prototype.__proto__ 指向构造函数(Object)的prototype属性
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
