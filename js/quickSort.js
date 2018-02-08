a9327f7ca892eafd12e9c310ffdf7eddc81c8438
Rambo <liguanliang@boyue.com>
2018-02-08 11:40:10 +0800

    -

---
+++ js/quickSort.js
@@ -0,0 +1,129 @@
+/**
+ *
+ *
+ * 快速排序
+ *
+ * */
+let times = 0;
+
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    let pivotIndex = Math.floor(arr.length / 2);
+    let pivot = arr.splice(pivotIndex, 1)[0];
+    let left = [];
+    let right = [];
+    for (let i = 0; i < arr.length; i++) {
+        times++;
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+    return quickSort(left).concat([pivot], quickSort(right));
+}
+
+let arr = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]; // 最不理想排序数组 -- 次数19次
+// let arr = [3, 7, 8, 5, 2, 1, 9, 5, 4];  // 定值的但无序的, 21次
+console.time('阮-Time');
+// let arr = makeRandomArr(); // 随机数字数组
+let result = quickSort(arr);
+console.log('阮-运算次数', times);
+console.log('阮-运算结果', result);
+console.timeEnd('阮-Time'); // 阮,时间 3-5ms
+
+// 交换元素位置
+function swap(array, i, k) {
+    let temp = array[i];
+    array[i] = array[k];
+    array[k] = temp;
+}
+// 我的冒泡排序
+function bubbleSort(arr) {
+    let result = arr.slice(0);
+    let length = result.length;
+    let times = 0;
+    for (let i = 0; i < length; i++) {
+        let nextLength = length - i;
+        let j = 1;
+        while (j < nextLength) {
+            let first = result[j - 1];
+            let second = result[j];
+            if (first > second) {
+                result[j] = first;
+                result[j - 1] = second;
+            }
+        }
+        swap(array, right, storeIndex); // 将基准元素放置到最后的正确位置上
+        // console.log('分区完成', storeIndex, array);
+        return storeIndex;
+    }
+}
+    // 排序递归
+    function sort(array, left, right) {
+        if (left >= right) {
+            return;
+        }
+        let storeIndex = partition(array, left, right);
+        sort(array, left, storeIndex - 1);
+        sort(array, storeIndex + 1, right);
+    }
+
+    sort(array, 0, array.length - 1);
+
+
+let arr = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+let result = insertSort(arr);
+console.log(result)
+// wiki 快速排序
+let wikiTimes = 0;
+function wikiQuickSort(array) {
+
+    // 交换元素位置
+    function swap(array, i, k) {
+        let temp = array[i];
+        array[i] = array[k];
+        array[k] = temp;
+    }
+
+    // 数组分区，左小右大
+    function partition(array, left, right) { // 遍历 (数组长度减一) 次
+        let storeIndex = left;
+        let pivot = array[right]; // 直接选最右边的元素为基准元素
+        for (let i = left; i < right; i++) {
+            wikiTimes++; // 运算次数计算
+            if (array[i] < pivot) {
+                swap(array, storeIndex, i);
+                storeIndex++; // 交换位置后，storeIndex 自增 1，代表下一个可能要交换的位置
+            }
+        }
+        swap(array, right, storeIndex); // 将基准元素放置到最后的正确位置上
+        // console.log('分区完成', storeIndex, array);
+        return storeIndex;
+    }
+
+    // 排序递归
+    function sort(array, left, right) {
+        if (left >= right) {
+            return;
+        }
+        let storeIndex = partition(array, left, right);
+        sort(array, left, storeIndex - 1);
+        sort(array, storeIndex + 1, right);
+    }
+
+    sort(array, 0, array.length - 1);
+
+    return array;
+}
+
+let wikiArr = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]; // 45次 ,最不理想排序数组 -- 次数
+// let wikiArr = [3, 7, 8, 5, 2, 1, 9, 5, 4]; // 随机的数字, 19次
+console.time('wikiTime');
+// let wikiArr = makeRandomArr(); // 随机数字数组
+let wikiResult = wikiQuickSort(wikiArr);
+console.log('wiki运算次数', wikiTimes);
+console.log('wiki运算结果', wikiResult);
+console.timeEnd('wikiTime');//wiki,时间 0-1.5ms 采用wiki
---
+++ js/watcher.js
@@ -0,0 +1,71 @@
+/**
+ *
+ * 观察者模式 订阅模式
+ *
+ * */
+// function PubSub() {
+//     this.handlers = {};
+// }
+// PubSub.prototype = {
+//     // 订阅事件
+//     on: function(eventType, handler){
+//         var self = this;
+//         if(!(eventType in self.handlers)) {
+//             self.handlers[eventType] = [];
+//         }
+//         self.handlers[eventType].push(handler);
+//         console.log(self.handlers.A);
+//         debugger;
+//         return this;
+//     },
+//     // 触发事件(发布事件)
+//     emit: function(eventType){
+//         var self = this;
+//         var handlerArgs = Array.prototype.slice.call(arguments,1);
+//         for(var i = 0; i < self.handlers[eventType].length; i++) {
+//             self.handlers[eventType][i].apply(self,handlerArgs);
+//         }
+//         return self;
+//     },
+//     // 删除订阅事件
+//     off: function(eventType, handler){
+//         var currentEvent = this.handlers[eventType];
+//         var len = 0;
+//         if (currentEvent) {
+//             len = currentEvent.length;
+//             for (var i = len - 1; i >= 0; i--){
+//                 if (currentEvent[i] === handler){
+//                     currentEvent.splice(i, 1);
+//                 }
+//             }
+//         }
+//         return this;
+//     }
+// };
+//
+// var pubsub = new PubSub();
+// var callback = function(data){
+//     console.log('第一次没有被删除事件',data);
+// };
+//
+// //订阅事件A 注册
+// pubsub.on('A', function(data){
+//     console.log(1 + data);
+// });
+// pubsub.on('A', function(data){
+//     console.log(2 + data);
+// });
+// pubsub.on('A', callback);
+//
+// //触发事件A
+// pubsub.emit('A', '我是参数','123');
+// // 1我是参数
+// // 2我是参数
+// // 第一次没有被删除事件 我是参数
+//
+// //删除事件A的订阅源callback
+// pubsub.off('A', callback);
+//
+// pubsub.emit('A', '我是第二次调用的参数');
+// //1我是第二次调用的参数
+// //2我是第二次调用的参数
