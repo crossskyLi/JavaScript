b0ebc60df4503e209afc77e05b08064703ed27e3
Rambo <liguanliang@boyue.com>
2018-02-10 16:01:32 +0800

    module语法

--- js/index.js
+++ js/index.js
@@ -10471,23 +10471,90 @@ $(function () {
     // 另外,export 语句输出的接口,与其对应的值是动态绑定关系
     // 即通过该接口,可以取到模块内部实时的值
 
-    export var foo = 'bar';
-    setTimeout(()=>foo = "baz",500)
+    // export var foo = 'bar';
+    // setTimeout(()=>foo = "baz",500)
     // 代码中输出变量foo ,值为bar,500毫秒后变成baz
 
+    // 这一点与CommonJS 规范完全不同,CommonJS 模块输出的是值的缓存
+    // 不存在动态更新
 
+    // 最后,export 命令可以出现在模块的任何位置,只要处于模块顶层就可以.
+    // 如果处于块级作用域内,就会报错,import 命令也是如此,
+    // 这是因为处于条件代码块之中,就没法做静态优化了,违背了ES6 的设计初衷
 
+    // function foo() {
+    //     export default 'bar'// SyntaxError
+    // }
+    // foo()
+    // 代码中,export 语句放在函数之中,结果报错
+    // --------------------------------------
 
+    // 4. import 命令
 
+    // 使用export 命令定义了模块的对外接口后,
+    // 其他JS文件可以通过import命令加载这个模块
 
+    // main.js
+    // import {firstName,lastName,year} from "./profile.js";
+    // function setName(element) {
+    //     element.textContent = firstName + lastName; // 引用
+    // }
 
+    // 代码的import命令,用于加载profile.js, 并从中输入变量,
+    // import 命令接受一对大括号,里面指定要从其他模块导入的变量名
+    // 大括号里面的变量名,必须与被导入模块(profile.js) 对外接口的名称相同
 
+    // 如果想为输入的变量重新取一个名字,import 命令要使用as 关键字
+    // 将输入的变量重命名
+    // import {lastName as surname} from './profile.js';
 
+    // import  命令输入的变量都是只读的,因为它的本质是输入接口,也就是说,
+    // 不允许在加载模块的脚本里,改写接口
+    // import {a} from './xxx.js';
+    // a = {} // Syntax Error: 'a' is read-only
+    // 代码中,脚本加载变量a,对其重新赋值就会报错
+    // 因为a 是一个只读的接口。
+    // 但是, 如果a 是一个对象,改写a 的属性是允许的
 
+    // import {a} from './xxx.js';
+    // a.foo = 'hello';
+    // 代码中,a的属性可以成功改写,并且其他模块也可以读到改写的值
+    // 不过这种写法很难查找错误,建议凡是输入的变量,都完全当作只读
+    // 轻易不要改变它的属性
 
+    // import 后面的from指定模块文件的位置,可以是相对路径,亦可以是绝对路径
+    // .js 后缀可以省略,若只是模块名字,不带有路径,那么必须有配置文件,
+    // 告诉JavaScript 引擎该模块的位置
+    // import {myMethod} from 'util';
+    // 代码中,util是模块文件名,由于不带有路径必须通过配置,告诉引擎怎么取到这个模块
 
+    // 注意,import 命令具有提升效果,会提升到整个模块的头部,首先执行
+    // foo();
+    // import {foo} from 'myModule';
+    // 代码不会报错,因为import 的执行早于foo的调用,这种行为的本质是
+    // import 命令是编译阶段执行的,在代码运行之前
 
+    // 由于 import 是静态执行,
+    // 所以不能使用表达式和变量,这些只有在运行时,才能得到结果的语法结构
 
+    // 报错
+    // import {'f'+'oo'} from 'myModule';// 报错
+
+    // 报错
+    // let module = "myModule";
+    // import {foo} from module;
+
+    // 报错
+    // if(x ===1){
+    //     import{foo}from "module1"
+    // }else{
+    //     import {foo} from 'module2'
+    // }
+    // 上面写法都会报错,因为它们用到了表达式,变量和if 结构
+    // 在静态分析阶段,这些语法都是没法得到值的
+    // 最后 ,import 语句会执行所加载的模块,因此可以有以下写法
+    import 'lodash'
+    // 上面代码仅仅是执行了lodash 模块,但是不输入任何值
 
 
 
