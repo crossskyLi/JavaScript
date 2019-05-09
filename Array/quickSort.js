c70c7c92e8ec79a448632e029249265ae16485c7
liguanliang <liguanliang@sensetime.com>
2019-05-09 10:17:16 +0800

    feat: array mergeSort pratice

--- Array/quickSort.js
+++ Array/quickSort.js
@@ -6,28 +6,28 @@
  * */
 // 生成一个有指定个数随机数字的数组,没有指定默认十个
 function makeRandomArr(arrLength) {
-    let length = 10;
-    let arr = [];
-    let random = 0;
-    let fixedNum = 0;
-    if (arrLength) {
-        length = arrLength
-    }
-    for (let i = 0; i < length; i++) {
-        random = length * Math.random();
-        fixedNum = parseInt(random) + 1;
-        arr.push(fixedNum)
-    }
-    return arr;
+  let length = 10;
+  let arr = [];
+  let random = 0;
+  let fixedNum = 0;
+  if (arrLength) {
+    length = arrLength
+  }
+  for (let i = 0; i < length; i++) {
+    random = length * Math.random();
+    fixedNum = parseInt(random) + 1;
+    arr.push(fixedNum)
+  }
+  return arr;
 }
 
 // 生成一个最不理想数字的数组
 function makeArr(arrLength) {
-    let arr = [];
-    for (let i = arrLength; i > 0; i--) {
-        arr.push(i)
-    }
-    return arr
+  let arr = [];
+  for (let i = arrLength; i > 0; i--) {
+    arr.push(i)
+  }
+  return arr
 }
 
 let arr = makeArr(10); // 最不理想排序数组
@@ -35,22 +35,22 @@ let randomArr = makeRandomArr(10);
 
 // 阮 快速排序
 function quickSort(array) {
-    let arr = array.concat();// 复制数组,不改变原来数组
-    if (arr.length <= 1) {
-        return arr;
+  let arr = array.concat();// 复制数组,不改变原来数组
+  if (arr.length <= 1) {
+    return arr;
+  }
+  let pivotIndex = Math.floor(arr.length / 2);
+  let pivot = arr.splice(pivotIndex, 1)[0];
+  let left = [];
+  let right = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
     }
-    let pivotIndex = Math.floor(arr.length / 2);
-    let pivot = arr.splice(pivotIndex, 1)[0];
-    let left = [];
-    let right = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] < pivot) {
-            left.push(arr[i]);
-        } else {
-            right.push(arr[i]);
-        }
-    }
-    return quickSort(left).concat([pivot], quickSort(right));
+  }
+  return quickSort(left).concat([pivot], quickSort(right));
 }
 
 console.time('阮-Time');
@@ -66,22 +66,22 @@ console.log('------------------------');
 
 // 冒泡排序
 function bubbleSort(arr) {
-    let result = arr.slice();
-    let length = result.length;
-    for (let i = 0; i < length; i++) {
-        let nextLength = length - i;
-        let j = 1;
-        while (j < nextLength) {
-            let first = result[j - 1];
-            let second = result[j];
-            if (first > second) {
-                result[j] = first;
-                result[j - 1] = second;
-            }
-            j++;
-        }
+  let result = arr.slice();
+  let length = result.length;
+  for (let i = 0; i < length; i++) {
+    let nextLength = length - i;
+    let j = 1;
+    while (j < nextLength) {
+      let first = result[j - 1];
+      let second = result[j];
+      if (first > second) {
+        result[j] = first;
+        result[j - 1] = second;
+      }
+      j++;
     }
-    return result;
+  }
+  return result;
 }
 
 console.time('bubble-Time');
@@ -99,41 +99,41 @@ console.log('------------------------');
 // wiki 快速排序
 function wikiQuickSort(array) {
 
-    // 交换元素位置
-    function swap(array, i, k) {
-        let temp = array[i];
-        array[i] = array[k];
-        array[k] = temp;
+  // 交换元素位置
+  function swap(array, i, k) {
+    let temp = array[i];
+    array[i] = array[k];
+    array[k] = temp;
+  }
+
+  // 数组分区，左小右大
+  function partition(array, left, right) { // 遍历 (数组长度减一) 次
+    let storeIndex = left;
+    let pivot = array[right]; // 直接选最右边的元素为基准元素
+    for (let i = left; i < right; i++) {
+      if (array[i] < pivot) {
+        swap(array, storeIndex, i);
+        storeIndex++; // 交换位置后，storeIndex 自增 1，代表下一个可能要交换的位置
+      }
     }
+    swap(array, right, storeIndex); // 将基准元素放置到最后的正确位置上
+    // console.log('分区完成', storeIndex, array);
+    return storeIndex;
+  }
 
-    // 数组分区，左小右大
-    function partition(array, left, right) { // 遍历 (数组长度减一) 次
-        let storeIndex = left;
-        let pivot = array[right]; // 直接选最右边的元素为基准元素
-        for (let i = left; i < right; i++) {
-            if (array[i] < pivot) {
-                swap(array, storeIndex, i);
-                storeIndex++; // 交换位置后，storeIndex 自增 1，代表下一个可能要交换的位置
-            }
-        }
-        swap(array, right, storeIndex); // 将基准元素放置到最后的正确位置上
-        // console.log('分区完成', storeIndex, array);
-        return storeIndex;
+  // 排序递归
+  function sort(array, left, right) {
+    if (left >= right) {
+      return;
     }
+    let storeIndex = partition(array, left, right);
+    sort(array, left, storeIndex - 1);
+    sort(array, storeIndex + 1, right);
+  }
 
-    // 排序递归
-    function sort(array, left, right) {
-        if (left >= right) {
-            return;
-        }
-        let storeIndex = partition(array, left, right);
-        sort(array, left, storeIndex - 1);
-        sort(array, storeIndex + 1, right);
-    }
+  sort(array, 0, array.length - 1);
 
-    sort(array, 0, array.length - 1);
-
-    return array;
+  return array;
 }
 
 console.time('wikiTime');
@@ -235,19 +235,31 @@ console.log('--------------------------')
 // let binaryResult2 = binarySearch(arr,6);
 // console.log('第二结果',binaryResult2);
 
+// 归并排序
 
+function merge(left, right) {
+  const result = [];
+  while (left.length > 0 && right.length > 0) {
+    if (left[0] < right[0]) {
+      result.push(left.shift())
+    } else {
+      result.push(right.shift())
+    }
+  }
+  return result;
+}
 
+function mergeSort(arr) {
+  const length = arr.length
 
+  if (length <= 1) return arr;
 
+  const middle = Math.floor(length / 2);
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle);
 
-
-
-
-
-
-
-
-
+  return merge(mergeSort(left), mergeSort(right))
+}
 
 
 
--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -6,10 +6,8 @@ const devConfig = Object.assign({
   mode,
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
-    port: 8080,
     publicPath: '/',
     hot: true,
-    host: "127.0.0.1",
     overlay: true, // 如果代码出错，会在浏览器页面弹出“浮动层”。类似于 vue-cli 等脚手架
     open: true,
   },
