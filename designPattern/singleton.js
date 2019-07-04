2d047f51f21739763fbac984d0da6f0aca5eef38
crossskyli <liguanliang1007@163.com>
2019-07-05 00:03:29 +0800

    feat: update

--- designPattern/singleton.js
+++ designPattern/singleton.js
@@ -8,7 +8,7 @@ function makeInvoker(func) {
   return result
 }
 function generator(...operators) {
-  function invoke(...args) {
+  function invoker(...args) {
     let _this;
     operators.forEach(operator => {
       _this = operator.apply(this, args)
@@ -16,28 +16,42 @@ function generator(...operators) {
     return _this;
   }
 
-  return invoke
+  return invoker
 }
-const insert = function (target, num) {
-  target.count = num;
-  return obj;
+// 函数重载 todo
+const insert = function (target, base) {
+  target.count = base || 0;
+  return target;
 }
 // 只运行一次
-const increase = function (target) {
-  ++target.count
+const increase = function (target, base, increaseNum) {
+  target.count += (increaseNum || 1)
   return target;
 }
 
-const decrease = function (target) {
-  --target.count;
+const decrease = function (target, base, increaseNum, decreaseNum) {
+  const _decreaseNum = decreaseNum || 1;
+  target.count -= _decreaseNum
+
   return target;
 }
 
-let invoker = makeInvoker(generator, insert, increase, decrease, increase, increase, increase)
-let invoker2 = makeInvoker(generator, insert, decrease, increase, increase)
-let obj = {};
-let count = 1;
-let result = invoker(obj, count);
-let result2 = invoker2(obj, count);
-console.log(result)
-console.log(result2 === result)
+function test() {
+  let obj = {};
+  let obj2 = {};
+  let base = 1;
+  let increaseNum = 3;
+  let decreaseNum = 2;
+  let invoker = makeInvoker(generator, insert, increase, decrease)
+  let invoker2 = makeInvoker(generator, insert, decrease, increase, increase)
+  let invoker3 = makeInvoker(generator, decrease)
+
+  let result = invoker(obj, base, 0, decreaseNum);
+  console.log(result)
+  let result2 = invoker2(obj2, base, increaseNum, decreaseNum);
+  let result3 = invoker3(obj, undefined, 3)
+  console.log(result2, result2 === result)
+  console.log(result3, result3 === result)
+}
+test();
+
